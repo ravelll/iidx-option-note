1c482d333eb241011937a40e7aad595b49879abd
Yoshihide Taniguchi (ravelll) <ravelll.taniguchi@gmail.com>
2020-02-12 15:02:02 +0900

    Done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const OptionNoteList = ({ notes }) => {
+  const list = notes.map((n, i) => {
+    return(
+      <div className="data-block" key={i}>
+        <div className="main-column">{ n.name }</div>
+        <div className="sub-column">OPTION: { n.option }</div>
+        { n.note !== "" &&
+          <div className="sub-column note-column">{ n.note }</div>
+        }
+      </div>
+    )
+  });
+
+  return <div className="data-container">{ list }</div>
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.optionNotes = require("./data/all.json");
+
+    this.state = {
+      displayedNotes: this.optionNotes,
+      inputVal: ''
+    }
+  }
+
+  handleChange = event => {
+    const inputVal = event.target.value;
+    let filtered = this.optionNotes.filter(n => {
+      return n.name_en.toLowerCase().includes(inputVal.toLowerCase());
+    });
+
+    const exactMatchedIndex = filtered.findIndex(data => {
+      return data.name_en !== "" && data.name_en.toLowerCase() === inputVal.toLowerCase();
+    });
+
+    if (exactMatchedIndex !== -1) {
+      let d = [filtered[exactMatchedIndex]];
+      filtered.splice(exactMatchedIndex, 1);
+      this.setState({ displayedNotes: d.concat(filtered) });
+    } else {
+      this.setState({ displayedNotes: filtered });
+    }
+
+    this.setState({ inputVal });
+  }
+
+  render() {
+    const inputVal = this.state.inputVal
+    return(
+      <div>
+        <div className="search-block">
+          <input type="url" name="inputVal" className="main-input" value={inputVal} onChange={this.handleChange} />
+        </div>
+        <OptionNoteList notes={this.state.displayedNotes} />
+      </div>
+    );
+  }
 }
 
 export default App;
