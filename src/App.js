bdeaac3c7df465ac88ad67577dc457e9c3b2937d
Yoshihide Taniguchi (ravelll) <ravelll.taniguchi@gmail.com>
2020-02-23 21:16:29 +0900

    use hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-const OptionNoteList = ({ notes }) => {
+const OptionNotesList = ({ notes }) => {
   const list = notes.map((n, i) => {
     return(
       <div className="data-block" key={i}>
@@ -17,64 +17,52 @@ const OptionNoteList = ({ notes }) => {
   return <div className="data-container">{ list }</div>
 }
 
-class App extends Component {
-  constructor() {
-    super();
+const App = () => {
+  const [displayedNotes, setDisplayedNotes] = useState([]);
+  const [defaultNotes, setDefaultNotes] = useState([]);
+  const [inputVal, setInputVal] = useState('');
 
-    this.state = {
-      displayedNotes: [],
-      defaultNotes: [],
-      inputVal: ''
+  useEffect(() => {
+    if (defaultNotes.length === 0) {
+      const notes = require("./data/all.json");
+      setDefaultNotes(notes);
+      setDisplayedNotes(notes);
+    }
+  }, [defaultNotes.length])
+
+  useEffect(() => {
+    const updateNotes = () => {
+      let filtered = defaultNotes.filter(n => {
+        return n.name_en.toLowerCase().includes(inputVal.toLowerCase());
+      });
+
+      const exactMatchedIndex = filtered.findIndex(data => {
+        return data.name_en !== "" && data.name_en.toLowerCase() === inputVal.toLowerCase();
+      });
+
+      if (exactMatchedIndex !== -1) {
+        const exactMatched = filtered.splice(exactMatchedIndex, 1);
+        setDisplayedNotes([...exactMatched, ...filtered]);
+      } else {
+        setDisplayedNotes(filtered);
+      }
     }
 
-    this.handleChange = this.handleChange.bind(this);
-    this.updateNote = this.updateNote.bind(this);
+    updateNotes();
+  }, [inputVal, defaultNotes]);
+
+  const handleChange = (e) => {
+    setInputVal(e.target.value);
   }
 
-  handleChange(event) {
-    this.setState({ inputVal: event.target.value });
-  }
-
-  updateNote() {
-    let filtered = this.state.defaultNotes.filter(n => {
-      return n.name_en.toLowerCase().includes(this.state.inputVal.toLowerCase());
-    });
-
-    const exactMatchedIndex = filtered.findIndex(data => {
-      return data.name_en !== "" && data.name_en.toLowerCase() === this.state.inputVal.toLowerCase();
-    });
-
-    if (exactMatchedIndex !== -1) {
-      const exactMatched = filtered.splice(exactMatchedIndex, 1);
-      console.log(exactMatched);
-      this.setState({ displayedNotes: [...exactMatched, ...filtered] });
-    } else {
-      this.setState({ displayedNotes: filtered });
-    }
-  }
-
-  componentDidUpdate(_, prevState) {
-    if (this.state.inputVal !== prevState.inputVal) {
-      this.updateNote();
-    }
-  }
-
-  componentDidMount() {
-    const notes = require("./data/all.json");
-    this.setState({ defaultNotes: notes });
-    this.setState({ displayedNotes: notes });
-  }
-
-  render() {
-    return(
-      <div>
-        <div className="search-block">
-          <input type="url" name="inputVal" className="main-input" value={this.state.inputVal} onChange={this.handleChange} />
-        </div>
-        <OptionNoteList notes={this.state.displayedNotes} />
+  return(
+    <div>
+      <div className="search-block">
+        <input type="url" name="inputVal" className="main-input" value={inputVal} onChange={handleChange} />
       </div>
-    );
-  }
+      <OptionNotesList notes={displayedNotes} />
+    </div>
+  );
 }
 
 export default App;
